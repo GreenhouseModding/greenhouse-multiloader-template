a2cc33f6b2a224301988aa4609bfbd6d2be38e08
Anastasia Calico <calico@merchantcalico.net>
2025-05-28 04:15:59 +1000

    chore: Bump dependency versions.

--- buildSrc/src/main/kotlin/house/greenhouse/examplemod/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/house/greenhouse/examplemod/gradle/Versions.kt
@@ -8,18 +8,18 @@ object Versions {
 	const val NEOFORM = "$MINECRAFT-20240808.144430"
 	const val PARCHMENT = "2024.07.28"
 
-	const val FABRIC_API = "0.106.0+1.21.1"
-	const val FABRIC_LOADER = "0.15.11"
+	const val FABRIC_API = "0.116.0+1.21.1"
+	const val FABRIC_LOADER = "0.16.14"
 	const val MOD_MENU = "11.0.3"
 	const val FABRIC_LOADER_RANGE = ">=0.15"
-	const val FABRIC_MINECRAFT_RANGE = ">=1.21"
+	const val FABRIC_MINECRAFT_RANGE = ">=1.21 <=1.21.1"
 
-	const val NEOFORGE = "21.1.73"
+	const val NEOFORGE = "21.1.173"
 	const val NEOFORGE_LOADER_RANGE = "[4,)"
-	const val NEOFORGE_MINECRAFT_RANGE = "[1.21,)"
+	const val NEOFORGE_MINECRAFT_RANGE = "[1.21,1.21.2)"
 
-	const val FABRIC_MIXIN = "0.15.0+mixin.0.8.7"
-	const val MIXIN_EXTRAS = "0.4.0"
+	const val FABRIC_MIXIN = "0.15.5+mixin.0.8.7"
+	const val MIXIN_EXTRAS = "0.4.1"
 
 	const val JAVA = 21
 }
